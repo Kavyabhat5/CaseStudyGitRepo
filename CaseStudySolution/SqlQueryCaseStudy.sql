a4c285f51353a2751776d5d908c3d47edd671b79
Kavya Bhat <Kavya.Bhat123@outlook.com>
2019-05-17 12:49:09 +0530

    Added Tables to the database

--- CaseStudySolution/SqlQueryCaseStudy.sql
+++ CaseStudySolution/SqlQueryCaseStudy.sql
@@ -1,1 +1,29 @@
-﻿
+﻿create database CaseStudySql
+Use CaseStudySql
+
+
+drop  table WorkOut_Category
+
+create table WorkOut_Category(
+Category_id int primary key identity,
+Category_Name varchar(64)
+)
+
+
+create table WorkOut_Collection(
+WorkOut_Title varchar(128),
+WorkOut_Note varchar(256),
+Calories_Burn_Per_Minute float,
+Category_id int foreign key references WorkOut_Category(Category_id), 
+WorkOut_id int  identity primary key
+)
+
+create table WorkOut_Active(
+WorkOut_id int foreign key references WorkOut_Collection(WorkOut_id),
+Start_time Time,
+Starts_Date date,
+End_Date date,
+End_Time time,
+Comment varchar(64),
+Statuss bit
+)
